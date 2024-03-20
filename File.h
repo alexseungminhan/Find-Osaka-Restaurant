bd85eaf1bb9961b455f055301d3e38dea3ecaf9e
Seungmin Han <164147387+alexseungminhan@users.noreply.github.com>
2024-03-20 18:42:35 +0900

    Update File.h

--- File.h
+++ File.h
@@ -8,12 +8,12 @@
 using namespace std;
 
 class Get_file {
-	Linked_list* list; //객체 생성
+	Linked_list* list; 
 public:
-	Get_file(string file_name); //매개 변수가 있는 생성자
-	vector<string> split_data(string line); //멤버 함수
+	Get_file(string file_name); 
+	vector<string> split_data(string line); 
 
-	Linked_list* get_list(); //리스트 주기
+	Linked_list* get_list(); 
 };
 
 #endif __FILE_H__
