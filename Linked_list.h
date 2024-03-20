27ce9caa9bd78d2913dbcaa0e336fceb1c36df6c
Seungmin Han <164147387+alexseungminhan@users.noreply.github.com>
2024-03-20 18:44:10 +0900

    Update Linked_list.h

--- Linked_list.h
+++ Linked_list.h
@@ -7,31 +7,31 @@
 
 using namespace std;
 
-typedef struct node { //식당 노드 생성
+typedef struct node { 
 	string name;
 	string address;
 	int rankingPosition;
 	int numberOfReviews;
 	vector<string> cuisines;
 
-	node* next; //링크드 리스트 형식
+	node* next; 
 
 } node;
 
 class Linked_list{
-	node* head; //헤드
-	node* tail; //테일
+	node* head; 
+	node* tail; 
 
 public:
 	Linked_list();
-	void add_list(node* new_node); //링크드 리스트 연결하기
-	void print_list(); //만들어진 링크드 리스트 출력
+	void add_list(node* new_node); 
+	void print_list(); 
 
-	Linked_list* find_list(string name, int sort,int filter); //이름으로 찾기
-	Linked_list* find_category(string category, int sort, int filter); //카테고리로 찾기
+	Linked_list* find_list(string name, int sort,int filter); 
+	Linked_list* find_category(string category, int sort, int filter); 
 	Linked_list* find_category(string category, int sort);
 
-	Linked_list* sort_by(Linked_list* list,int sort); //정렬하기
+	Linked_list* sort_by(Linked_list* list,int sort); 
 
 };
 
