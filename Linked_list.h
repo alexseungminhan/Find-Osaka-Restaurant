ac78038ca855f1b3ea2dc54cd3721c62ffd10237
Seungmin Han <164147387+alexseungminhan@users.noreply.github.com>
2024-03-25 16:09:12 +0900

    Add files via upload

--- Linked_list.h
+++ Linked_list.h
@@ -7,31 +7,33 @@
 
 using namespace std;
 
-typedef struct node { 
+typedef struct node { //식당 노드 생성
 	string name;
 	string address;
 	int rankingPosition;
 	int numberOfReviews;
 	vector<string> cuisines;
 
-	node* next; 
+	node* next; //링크드 리스트 형식
 
 } node;
 
 class Linked_list{
-	node* head; 
-	node* tail; 
+	node* head; //헤드
+	node* tail; //테일
 
 public:
 	Linked_list();
-	void add_list(node* new_node); 
-	void print_list(); 
+	void add_list(node* new_node); //링크드 리스트 연결하기
+	void print_list(); //만들어진 링크드 리스트 출력
 
-	Linked_list* find_list(string name, int sort,int filter); 
-	Linked_list* find_category(string category, int sort, int filter); 
-	Linked_list* find_category(string category, int sort);
+	Linked_list* find_list(string name, int sort1,int sort2,int filter); //이름으로 찾기
+	Linked_list* find_category(string category, int sort1, int sort2, int filter); //카테고리로 찾기
+	Linked_list* find_category(string category, int sort1, int sort2);
 
-	Linked_list* sort_by(Linked_list* list,int sort); 
+	Linked_list* sort_by(Linked_list* list,int sort1, int sort2); //정렬하기
+
+	Linked_list* resverse_list(Linked_list* list); 
 
 };
 
--- Source.cpp
+++ Source.cpp
@@ -6,16 +6,26 @@ using namespace std;
 
 int main() {
 	Get_file* file = new Get_file("osaka_tripadvisor.csv");
-	Linked_list* osaka_list = file->get_list(); 
+	Linked_list* osaka_list = file->get_list(); //file의 리스트를 가져와서 osaka_list에 넣기
 
-	Linked_list* result = new Linked_list; 
+	Linked_list* result = new Linked_list; //검색결과를 저장할 링크드 리스트 result
 
 	cout << "\033[31m--------Tripadvisor's Top 500 Osaka Restaurant--------\033[0m" << endl;
+
+	
+
+	int sort2 ;
 	cout << "sort by 0.no sort 1. ranking 2.number of reviews" << endl;
 	cout << "\033[36msort by:\033[0m";
+	cin >> sort2;
 
-	int sort;
-	cin >> sort;
+	int sort1=0;
+	if (sort2 != 0) {
+		cout << "sort by  1. ascending order 2.descending order" << endl;
+		cout << "\033[36msort by:\033[0m";
+		
+		cin >> sort1;
+	}
 
 	cout << "filter: 0. no filter 1.vegan option 2.vegeterian friendly 3. gluten free" << endl;
 	cout << "\033[36mfilter:\033[0m";
@@ -23,24 +33,29 @@ int main() {
 	int filter;
 	cin >> filter;
 
-	cout << "1. find by name 2. find by category" << endl;
+	cout << "Search: 0.search all 1. find by name 2. find by category" << endl;
 	cout << "\033[36mfind by:\033[0m";
 
 	int select;
 	cin >> select;
 
+	if (select == 0) {
+		result = osaka_list->sort_by(osaka_list, sort1, sort2);
+		result->print_list();
+	}
+
 	if (select == 1) {
 		cout << "\033[36mname:\033[0m";
 		string name;
 		cin >> name;
-		result = osaka_list->find_list(name,sort,filter);
+		result = osaka_list->find_list(name,sort1,sort2,filter);
 		result->print_list();
 	}
 	if (select == 2) {
 		cout << "\033[36mcategory:\033[0m";
 		string category;
 		cin >> category;
-		result = osaka_list->find_category(category,sort,filter);
+		result = osaka_list->find_category(category,sort1,sort2,filter);
 		result->print_list();
 	}
 
