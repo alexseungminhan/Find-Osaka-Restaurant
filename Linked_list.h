96dc6a2789afdb30717a97d923460102966f6c7a
Seungmin Han <164147387+alexseungminhan@users.noreply.github.com>
2024-03-25 21:12:49 +0900

    add comment

--- Linked_list.h
+++ Linked_list.h
@@ -7,33 +7,38 @@
 
 using namespace std;
 
-typedef struct node { //식당 노드 생성
+typedef struct node { //restaurant node
 	string name;
 	string address;
 	int rankingPosition;
 	int numberOfReviews;
 	vector<string> cuisines;
+	//cuisines include each restaurants' categories
 
-	node* next; //링크드 리스트 형식
+	node* next; 
 
 } node;
 
 class Linked_list{
-	node* head; //헤드
-	node* tail; //테일
+	node* head; 
+	node* tail; 
 
 public:
 	Linked_list();
-	void add_list(node* new_node); //링크드 리스트 연결하기
-	void print_list(); //만들어진 링크드 리스트 출력
+	void add_list(node* new_node); //add node to linked list
+	void print_list(); //print entire linked list
 
-	Linked_list* find_list(string name, int sort1,int sort2,int filter); //이름으로 찾기
-	Linked_list* find_category(string category, int sort1, int sort2, int filter); //카테고리로 찾기
-	Linked_list* find_category(string category, int sort1, int sort2);
+	Linked_list* find_list(string name, int sort1,int sort2,int filter); //find list by name
+	//:param filter: search list by name or category
+	Linked_list* find_category(string category, int sort1, int sort2, int filter); //find list by category
+	Linked_list* find_category(string category, int sort1, int sort2); //fucntion overloading
 
-	Linked_list* sort_by(Linked_list* list,int sort1, int sort2); //정렬하기
+	Linked_list* sort_by(Linked_list* list,int sort1, int sort2); 
+	//sort list 
+	//:param sort1: search by descending or ascending order
+	//:param sort2: search by ranking or number of reviews
 
-	Linked_list* resverse_list(Linked_list* list); 
+	Linked_list* resverse_list(Linked_list* list); //reverse linked list in order
 
 };
 
